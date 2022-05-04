a6d487c0eda8b97220d492b520e5032eb888c2e6
Alessandrostr95 <alessandrostr95@gmail.com>
2022-05-05 00:00:27 +0200

    added class images api

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -1,6 +1,9 @@
 package routes
 
 import (
+	"net/http"
+	"os"
+
 	"github.com/gorilla/mux"
 
 	"informatica.uniroma2.eu/api/controllers"
@@ -11,4 +14,13 @@ func RegisterRoutes(router *mux.Router) {
 	router.HandleFunc("/api/data/{cdl}/{year}/class", controllers.GetAllClasses).Methods("GET")
 	router.HandleFunc("/api/data/{cdl}/{year}/calendar/{semester}", controllers.GetCalendarBySemester).Methods("GET")
 	router.HandleFunc("/api/data/news", controllers.GetNews).Methods("GET")
+	router.HandleFunc("/api/data/images/{img}", func(w http.ResponseWriter, r *http.Request) {
+		path := "static/images/" + mux.Vars(r)["img"]
+		if _, err := os.Stat(path); err == nil {
+			http.ServeFile(w, r, path)
+		} else {
+			http.ServeFile(w, r, "static/images/404.jpg")
+		}
+		return
+	}).Methods("GET")
 }
